7701578a7c96a376cc510444c23d528192590f90
Matheus Curvelo <83092595+matheus-curvelo@users.noreply.github.com>
2024-05-24 18:50:35 -0300

    Apagando arquivos

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/main.scss";
-import Header from "./templates/Header";
-import Banner from "./templates/Banner";
-import AboutMe from "./templates/AboutMe";
-import Skills from "./templates/Skills";
-import Footer from "./templates/Footer";
-
-import reportWebVitals from "./reportWebVitals";
-
-//Bootstrap 5.1.3
-import "bootstrap/dist/css/bootstrap.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Header />
-    <Banner />
-    <AboutMe />
-    <Skills />
-    <Footer />
+    <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
