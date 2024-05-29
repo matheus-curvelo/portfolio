ec48cd9c37af015de04d69a860e9973920717695
Matheus Curvelo <83092595+matheus-curvelo@users.noreply.github.com>
2024-05-29 02:37:10 -0300

    Criando componente AboutIcons

---
+++ src/components/AboutIcons/AboutIcons.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./AboutIcons.scss";
+import "animate.css";
+import {Box} from "@mui/material";
+
+import {ReactComponent as HtmlIcon} from "../../assets/svg/html.svg";
+import {ReactComponent as CssIcon} from "../../assets/svg/css.svg";
+import {ReactComponent as JavaScriptIcon} from "../../assets/svg/javascript.svg";
+import {ReactComponent as BootstrapIcon} from "../../assets/svg/bootstrap.svg";
+import {ReactComponent as SassIcon} from "../../assets/svg/sass.svg";
+import {ReactComponent as MaterialUIIcon} from "../../assets/svg/mui.svg";
+import {ReactComponent as ReactIcon} from "../../assets/svg/react.svg";
+import {ReactComponent as ReactNativeIcon} from "../../assets/svg/reactnative.svg";
+import {ReactComponent as NextJsIcon} from "../../assets/svg/nextjs.svg";
+import {ReactComponent as NodeJsIcon} from "../../assets/svg/nodejs.svg";
+import {ReactComponent as GitHubIcon} from "../../assets/svg/github.svg";
+import {ReactComponent as GitIcon} from "../../assets/svg/git.svg";
+import {ReactComponent as TypeScriptIcon} from "../../assets/svg/typescript.svg";
+import {ReactComponent as WordPressIcon} from "../../assets/svg/wordpress.svg";
+import {ReactComponent as DockerIcon} from "../../assets/svg/docker.svg";
+import {ReactComponent as AxiosIcon} from "../../assets/svg/axios.svg";
+
+const icons = [
+  {component: HtmlIcon, name: "HTML"},
+  {component: CssIcon, name: "CSS"},
+  {component: JavaScriptIcon, name: "JavaScript"},
+  {component: BootstrapIcon, name: "Bootstrap"},
+  {component: SassIcon, name: "Sass"},
+  {component: MaterialUIIcon, name: "Material UI"},
+  {component: ReactIcon, name: "React"},
+  {component: ReactNativeIcon, name: "React Native"},
+  {component: NextJsIcon, name: "NextJs"},
+  {component: NodeJsIcon, name: "NodeJs"},
+  {component: GitHubIcon, name: "GitHub"},
+  {component: GitIcon, name: "Git"},
+  {component: TypeScriptIcon, name: "TypeScript"},
+  {component: WordPressIcon, name: "WordPress"},
+  {component: DockerIcon, name: "Docker"},
+  {component: AxiosIcon, name: "Axios"},
+];
+
+const AboutIcons: React.FC = () => {
+  return (
+    <Box component="div" className="icons">
+      {icons.map(({component: IconComponent, name}, index) => (
+        <Box component="div" className="icon">
+          <IconComponent className="icon__svg" width="50" height="50" />
+          {name}
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+export default AboutIcons;
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,14 +1,43 @@
+// import React from "react";
+// import "./About.scss";
+// import {Box, Container, Typography} from "@mui/material";
+
+// const About: React.FC = () => {
+//   return (
+//     <Box id="about" component="section" py={8}>
+//       <Container maxWidth="xl">
+//         <Box className="about__box" component="div">
+//           <Typography variant="h1">Sobre</Typography>
+//         </Box>
+//       </Container>
+//     </Box>
+//   );
+// };
+
+// export default About;
+
 import React from "react";
 import "./About.scss";
 import {Box, Container, Typography} from "@mui/material";
+import AboutIcons from "../../components/AboutIcons";
 
 const About: React.FC = () => {
   return (
     <Box id="about" component="section" py={8}>
       <Container maxWidth="xl">
-        <Box className="about__box" component="div">
-          <Typography variant="h1">Sobre</Typography>
-        </Box>
+        <Typography variant="h2" gutterBottom>
+          Sobre Mim
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Olá, sou Matheus, um desenvolvedor front-end e futuro engenheiro de
+          software apaixonado por tecnologia. Com quase 3 anos de experiência em
+          desenvolvimento web, estou sempre em busca de novas oportunidades e
+          colaborações inovadoras.
+        </Typography>
+        <Typography variant="h4" gutterBottom>
+          Skills
+        </Typography>
+        <AboutIcons />
       </Container>
     </Box>
   );
